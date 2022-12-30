6e8057ea63df07dd4fd9ae6e0004fc108c2ebcdb
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2022-12-30 13:16:12 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+if __name__ == '__main__':
+    from sys import argv
+
+    args = argv[1:]
+    
+    sums = 0
+
+    for value in argv[1:]:
+        sums += int(value)
+
+    print("{}".format(sums))

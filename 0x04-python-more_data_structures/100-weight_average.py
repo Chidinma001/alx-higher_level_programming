a0ca1a8d5608654a4dc6cf007e17ad73c8a6ba47
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2023-01-23 14:15:21 +0100

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,10 @@
-
+#!/src/bin/python3
+def weight_average(my_list=[]):
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+    prod = 0
+    wt = 0
+    for ele in my_list:
+        prod += (ele[0] * ele[1])
+        wt += ele[1]
+    return (prod / wt)

2abbe75cb7cfb1610ca595c28e781e31f0d6a686
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2023-01-30 12:50:47 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-   try:
+    try:
         count = 0
         while count < x:
             print("{}".format(my_list[count]), end='')
             count += 1
-         print()
+        print()
         return count
     except IndexError:
         print()

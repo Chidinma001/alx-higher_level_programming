83fab945c8de208e6849f8c28b5966af55c43cc9
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2023-02-13 12:37:40 +0100

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-'''
-module for printing a square with th string '#'
-'''
-def print_square(size):
-    '''
-    
-    :param size: the size that determines the size of the square
-    :return: returns a sqaure the size of 'size'
-    :raise: if size is less than 0, raise a ValueError, if size is not an integer and if size is a float
-    '''
-    if not isinstance(size, int):
-       raise TypeError("size must be an integer")
-    elif isinstance(size,(float)) and size <= 0:
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
+"""The print square module."""
 
-    for i in range(size):
-        box = '#'
-        print(box * size)
-        print(end ='')
+def print_square(size):
+    """Inside the function.
+    
+    Args:
+        size (int): size of square.
+    Raises:
+        TypeError and ValueError
+    """
+    if isinstance(size, float) and size < 0.0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if isinstance(size, int) and size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, type(True)):
+        raise TypeError("size must be an integer")
+
+    for _ in range(size):
+        for _ in range(size):
+            print("#", end="")
+        print("")

58b355737a15a05bf65d82be666c9d989d9bc48a
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2023-03-25 13:47:45 +0100

    Update relationship_city.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 
 class City(Base):
     """Represents a city for a MySQL database.
+    
     Attributes:
         id (sqlalchemy.Column): The city's id.
         name (sqlalchemy.Column): The city's name.

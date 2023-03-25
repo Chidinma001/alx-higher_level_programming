40eee859e263559d449a8ea9fef77ea461db180a
Chidinma001 <111283793+Chidinma001@users.noreply.github.com>
2023-03-25 13:20:31 +0100

    Update 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module called 0-select_states"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states`")
+    list(map(print, sorted(cur.fetchall(), key=lambda x: x[0])))
+    cur.close()
+    db.close()
